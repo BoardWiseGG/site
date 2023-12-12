0c7d2b5932f06912034d8da3d13008cc53c50245
Joshua Potter <jrpotter2112@gmail.com>
2023-12-12 08:30:20 -0700

    Have contact refer to github repo.

--- assets/js/react/pages/Contact.tsx
+++ assets/js/react/pages/Contact.tsx
@@ -6,7 +6,17 @@ export function Contact() {
   return (
     <>
       <PageIntro eyebrow="Contact Us" title="Questions or Comments?">
-        <p>Tell us how we can improve our site.</p>
+        <p>
+          Tell us how we can improve our site by leaving an issue at{" "}
+          <a
+            target="_blank"
+            className="underline"
+            href="https://github.com/boardwise-gg/website"
+          >
+            our GitHub repo
+          </a>
+          .
+        </p>
       </PageIntro>
     </>
   )
